a53c93c10484389adfd8ba059efa348edf6ecbc0
OlegB1 <olegburduzhan@gmail.com>
2018-07-09 22:28:39 +0300

    fix smth

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const path = require('path')
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, '')))
-    .set('views', path.join(__dirname, ''))
+app.use(express.static(path.join(__dirname)))
+    .set('views', path.join(__dirname))
 http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 let history = [];
