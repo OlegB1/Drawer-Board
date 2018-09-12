5ef2c303fe3ce0164d1f1690879e3aba4c22d45a
OlegB1 <olegburduzhan@gmail.com>
2018-09-12 23:37:43 +0300

    check mobile draw

--- draw.js
+++ draw.js
@@ -1,4 +1,4 @@
-let startX, startY, mouseX, mouseY, colorPickerWidth, canvasWidth;
+let startX, startY, endX, endY, canvasWidth, rect;
 let red = 255;
 let green = 255;
 let blue = 255;
@@ -10,7 +10,6 @@ let stage = 0;
 const socket = io();
 const canvas = document.getElementsByTagName('canvas')[0];
 const ctx = canvas.getContext("2d");
-const rect = canvas.getBoundingClientRect();
 window.onload = () => {
     socket.emit('drawLine', 'getData');
 };
@@ -18,7 +17,7 @@ window.onload = () => {
 document.getElementsByClassName("colorImg")[0].style.background = customColor;
 
 
-canvasWidth = canvas.parentElement.offsetWidth -30 + 'px';
+canvasWidth = canvas.parentElement.offsetWidth - 30 + 'px';
 canvasHeight = document.getElementsByClassName('color-picker')[0].offsetHeight + 'px';
 
 canvas.setAttribute('width', canvasWidth);
@@ -36,58 +35,20 @@ socket.on('drawLine', (data) => {
             data.splice(data.length - stage, stage);
         }
         for (let i = 0; i < data.length; i++) {
-            if (data[i].length >= 1 && data[i][0].and) {
+            if (data[i].length >= 1 && data[i][0].end) {
                 for (let j = 0; j < data[i].length; j++) {
-                    ctx.beginPath();
-                    ctx.strokeStyle = data[i][j].color;
-                    ctx.moveTo(data[i][j].start.startX, data[i][j].start.startY);
-                    ctx.lineTo(data[i][j].and.mouseX, data[i][j].and.mouseY);
-                    ctx.closePath();
-                    ctx.stroke();
+                    drawLine(data[i][j].start.startX, data[i][j].start.startY,
+                        data[i][j].end.endX, data[i][j].end.endY, data[i][j].color);
                 }
-            } else if (!data[i][0].and) {
+            } else if (!data[i][0].end) {
                 drawCircle(data[i][0].start.startX, data[i][0].start.startY, data[i][0].color);
             }
         }
     } else ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
-mouseCordinate = (event) => {
-    event.preventDefault();
-    if (event.type != 'touchmove'){
-        mouseX = event.clientX - rect.left;
-        mouseY = event.clientY - rect.top;
-    }
-    if (event.type == 'touchmove'){
-        mouseX = event.touches[0].clientX - rect.left;
-        mouseY = event.touches[0].clientY - rect.top;
-    }
-    let points = {start: {startX, startY}, and: {mouseX, mouseY}, color: customColor};
-    mouseCordinates.push(points);
-    ctx.strokeStyle = customColor;
-    ctx.beginPath();
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(mouseX, mouseY);
-    ctx.closePath();
-    ctx.stroke();
-    startX = mouseX;
-    startY = mouseY;
-
-};
-
-drawCircle = (x, y, color) => {
-    let radius = ctx.lineWidth / 2;
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = color;
-    ctx.fill();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = color;
-    ctx.stroke();
-    ctx.lineWidth = 7;
-};
-
 canvas.addEventListener('mousedown', (event) => {
+    rect = canvas.getBoundingClientRect();
     startX = event.clientX - rect.left;
     startY = event.clientY - rect.top;
     isDrow = true;
@@ -95,11 +56,12 @@ canvas.addEventListener('mousedown', (event) => {
 });
 
 canvas.addEventListener('touchstart', (event) => {
+    rect = canvas.getBoundingClientRect();
     startX = event.touches[0].clientX - rect.left;
     startY = event.touches[0].clientY - rect.top;
     isDrow = true;
-    canvas.addEventListener('touchmove', mouseCordinate,false);
-},false);
+    canvas.addEventListener('touchmove', mouseCordinate, false);
+}, false);
 
 window.addEventListener('touchend', (event) => {
     if (!isDrow) {
@@ -107,10 +69,12 @@ window.addEventListener('touchend', (event) => {
     }
     if (isDrow) {
         if (mouseCordinates.length && !stage) {
+            document.body.style.overflowY = 'auto';
             if (mouseCordinates.length > 1) {
                 socket.emit('drawLine', mouseCordinates);
             }
         } else if (!mouseCordinates.length) {
+            document.body.style.overflowY = 'auto';
             drawCircle(startX, startY, customColor);
             mouseCordinates.push({start: {startX, startY}, color: customColor});
             socket.emit('drawLine', mouseCordinates);
@@ -147,6 +111,45 @@ window.addEventListener("mouseup", () => {
         stage = 0;
     }
 });
+
+mouseCordinate = (event) => {
+    if (event.type != 'touchmove') {
+        endX = event.clientX - rect.left;
+        endY = event.clientY - rect.top;
+    }
+    if (event.type == 'touchmove') {
+        endX = event.touches[0].clientX - rect.left;
+        endY = event.touches[0].clientY - rect.top;
+        document.body.style.overflowY = 'hidden';
+        event.preventDefault();
+    }
+    let point = {start: {startX, startY}, end: {endX, endY}, color: customColor};
+    mouseCordinates.push(point);
+    drawLine (startX, startY, endX, endY, customColor);
+    [startX, startY] = [endX, endY];
+};
+
+
+drawCircle = (x, y, color) => {
+    let radius = ctx.lineWidth / 2;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.lineWidth = 7;
+};
+
+drawLine = (startX, startY, endX, endY, color) => {
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(endX, endY);
+    ctx.closePath();
+    ctx.stroke();
+}
 
 setColor = (ev) => {
     if (ev.name == 'red') {
